84802f0f46a128385b194453c46ec6b104539ca5
Lahari Narravula <nlahari1306@gmail.com>
2025-07-24 10:13:07 +0530

    commit

--- Insert_Scripts/Insert_Scripts.sql
+++ Insert_Scripts/Insert_Scripts.sql
@@ -258,83 +258,47 @@ VALUES (1050, 'C1', 'Backend Developer', 'Express', 787219, 1023563, 4, 9);
 ----candidates inserts
 INSERT INTO candidates VALUES (1, 'Arjun', 'Reddy', 'arjun.reddy@gmail.com', 9876543210, TO_DATE('1993-05-22','YYYY-MM-DD'), 'Passport', 'K1234567', 'B.Tech', 'IIT Madras', 8.9, 'Hyderabad', 'India', 'Infosys', 800000, 1000000, 5, 'Java', 'In Progress', NULL, 'Active', 'M','Software Engineer');
 INSERT INTO candidates VALUES (2, 'Priya', 'Kumar', 'priya.kumar@gmail.com', 9876543211, TO_DATE('1994-08-15','YYYY-MM-DD'), 'Aadhar', 456123789897, 'M.Tech', 'IIT Bombay', 9.1, 'Mumbai', 'India', 'TCS', 900000, 1100000, 6, 'PL/SQL', 'In Progress', NULL, 'Active', 'F','Backend Developer');
-
 INSERT INTO candidates VALUES (3, 'Rahul', 'Verma', 'rahul.verma@gmail.com', 9876543212, TO_DATE('1992-11-10','YYYY-MM-DD'), 'DL', 'MH0120160123456', 'B.E.', 'BITS Pilani', 8.5, 'Pune', 'India', 'Wipro', 850000, 950000, 4, 'Angular', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (4, 'Sneha', 'Sharma', 'sneha.sharma@gmail.com', 9876543213, TO_DATE('1995-03-30','YYYY-MM-DD'), 'Passport', 'V1234567', 'B.Tech', 'NIT Trichy', 9.0, 'Delhi', 'India', 'HCL', 870000, 1050000, 5, 'Devops', 'In Progress', NULL, 'Active', 'F','Devops Engineer');
-
 INSERT INTO candidates VALUES (5, 'Kiran', 'Rao', 'kiran.rao@gmail.com', 9876543214, TO_DATE('1990-12-05','YYYY-MM-DD'), 'Passport', 'K1984567', 'B.Sc', 'Osmania University', 7.8, 'Hyderabad', 'India', 'Infosys', 600000, 800000, 3, 'Java', 'Hired', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (6, 'Meena', 'Joshi', 'meena.joshi@gmail.com', 9876543215, TO_DATE('1991-07-18','YYYY-MM-DD'), 'Aadhar', 159753456123, 'MCA', 'Delhi University', 8.2, 'Delhi', 'India', 'IBM', 750000, 950000, 4, 'PL/SQL', 'Rejected', 'Lack of experience', 'Inactive', 'F','Software Engineer');
-
 INSERT INTO candidates VALUES (7, 'Vikas', 'Shetty', 'vikas.shetty@gmail.com', 9876543216, TO_DATE('1993-04-20','YYYY-MM-DD'), 'DL', 'KA0520197896543', 'B.Tech', 'JNTU', 7.5, 'Bangalore', 'India', 'Capgemini', 700000, 850000, 3, 'Angular', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (8, 'Neha', 'Kulkarni', 'neha.kulkarni@gmail.com', 9876543217, TO_DATE('1992-09-12','YYYY-MM-DD'), 'Passport', 'M2345678', 'B.E.', 'VIT', 8.6, 'Chennai', 'India', 'TCS', 800000, 1000000, 5, 'Django', 'In Progress', NULL, 'Active', 'F','Backend Developer');
-
 INSERT INTO candidates VALUES (9, 'Rohan', 'Desai', 'rohan.desai@gmail.com', 9876543218, TO_DATE('1994-01-25','YYYY-MM-DD'), 'Aadhar', 456789123654, 'M.Tech', 'IIT Kanpur', 9.2, 'Ahmedabad', 'India', 'L T', 900000, 1100000, 6, 'Java', 'In Progress', NULL, 'Active', 'M','Software Engineer');
--- 10 already valid
 INSERT INTO candidates VALUES (10, 'Aisha', 'Khan', 'aisha.khan@gmail.com', 9876543219, TO_DATE('1995-10-11','YYYY-MM-DD'), 'DL', 'UP1020174567890', 'BCA', 'IGNOU', 7.2, 'Lucknow', 'India', 'Infosys', 600000, 750000, 2, 'PLSQL', 'In Progress', NULL, 'Active', 'F','Backend Developer');
-
 INSERT INTO candidates VALUES (11, 'Raj', 'Kapoor', 'raj.kapoor@gmail.com', 9876543220, TO_DATE('1993-06-19','YYYY-MM-DD'), 'DL', 'DL0420153692581', 'B.Tech', 'IIT Delhi', 8.8, 'Delhi', 'India', 'TCS', 850000, 950000, 5, 'Angular', 'Rejected', 'Skill mismatch', 'Inactive', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (12, 'Divya', 'Mehta', 'divya.mehta@gmail.com', 9876543221, TO_DATE('1991-03-09','YYYY-MM-DD'), 'Passport', 'H7531598', 'MBA', 'Symbiosis', 8.3, 'Pune', 'India', 'Infosys', 920000, 1050000, 6, 'UI/UX', 'In Progress', NULL, 'Active', 'F','UI/UX Designer');
-
 INSERT INTO candidates VALUES (13, 'Sahil', 'Bhatia', 'sahil.bhatia@gmail.com', 9876543222, TO_DATE('1992-02-22','YYYY-MM-DD'), 'Aadhar', 951357456123, 'B.Tech', 'SRM University', 7.9, 'Chennai', 'India', 'Cognizant', 780000, 880000, 4, 'Java', 'Hired', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (14, 'Pooja', 'Singh', 'pooja.singh@gmail.com', 9876543223, TO_DATE('1994-11-30','YYYY-MM-DD'), 'DL', 'AS0120172587413', 'M.Tech', 'IIT Guwahati', 9.0, 'Guwahati', 'India', 'Wipro', 850000, 950000, 5, 'PL/SQL', 'Rejected', 'Interview not cleared', 'Inactive', 'F','Product Manager');
-
 INSERT INTO candidates VALUES (15, 'Anil', 'Menon', 'anil.menon@gmail.com', 9876543224, TO_DATE('1990-05-13','YYYY-MM-DD'), 'Aadhar', 654987321987, 'B.Sc', 'Kerala University', 7.0, 'Kochi', 'India', 'Infosys', 580000, 700000, 3, 'Angular', 'In Progress', NULL, 'Active', 'M','Product Manager');
-
 INSERT INTO candidates VALUES (16, 'Snehal', 'Patil', 'snehal.patil@gmail.com', 9876543225, TO_DATE('1993-07-27','YYYY-MM-DD'), 'DL', 'MH0120161479632', 'BCA', 'Mumbai University', 7.8, 'Mumbai', 'India', 'IBM', 720000, 820000, 4, 'PLSQL', 'Hired', NULL, 'Active', 'F','Product Manager');
-
 INSERT INTO candidates VALUES (17, 'Ravi', 'Chandra', 'ravi.chandra@gmail.com', 9876543226, TO_DATE('1991-09-15','YYYY-MM-DD'), 'Aadhar', 951753852654, 'B.Tech', 'IIT Hyderabad', 8.5, 'Hyderabad', 'India', 'TCS', 880000, 980000, 5, 'Java', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (18, 'Tanvi', 'Naik', 'tanvi.naik@gmail.com', 9876543227, TO_DATE('1992-04-08','YYYY-MM-DD'), 'DL', 'TS1020183219876', 'MCA', 'NIT Warangal', 8.7, 'Warangal', 'India', 'Accenture', 870000, 970000, 4, 'PL/SQL', 'In Progress', NULL, 'Active', 'F','Backend Developer');
-
 INSERT INTO candidates VALUES (19, 'Kunal', 'Joshi', 'kunal.joshi@gmail.com', 9876543228, TO_DATE('1994-08-21','YYYY-MM-DD'), 'Passport', 'J9631472', 'B.Tech', 'IIT Roorkee', 9.0, 'Roorkee', 'India', 'Tech Mahindra', 890000, 1000000, 6, 'Angular', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (20, 'Bhavana', 'Rathi', 'bhavana.rathi@gmail.com', 9876543229, TO_DATE('1993-12-03','YYYY-MM-DD'), 'DL', 'UP1120197534561', 'MBA', 'Amity University', 8.1, 'Noida', 'India', 'Infosys', 760000, 860000, 3, 'Sales', 'Rejected', 'Communication issues', 'Inactive', 'F','Sales Manager');
-
 INSERT INTO candidates VALUES (21, 'Gaurav', 'Singh', 'gaurav.singh@gmail.com', 9876543230, TO_DATE('1991-05-05','YYYY-MM-DD'), 'Passport', 'P6678899', 'M.Tech', 'BITS Pilani', 8.9, 'Noida', 'India', 'Adobe', 1000000, 1200000, 9, 'Java', 'Rejected', 'Failed tech round', 'Inactive', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (22, 'Lavanya', 'Menon', 'lavanya.menon@gmail.com', 9876543231, TO_DATE('1996-07-17','YYYY-MM-DD'), 'DL', 334455778, 'MBA', 'IGNOU', 7.6, 'Pune', 'India', 'HDFC', 400000, 600000, 2, 'Angular', 'In Progress', NULL, 'Active', 'F','Software Engineer');
-
-INSERT INTO candidates VALUES (23, 'Nikhil', 'Kumar', 'nikhil.kumar@gmail.com', 9876543232, TO_DATE('1994-09-09','YYYY-MM-DD'), 'Aadhar', 111333555, 'BCA', 'Amity University', 7.1, 'Lucknow', 'India', 'Infosys', 550000, 750000, 5, 'PL/SQL', 'Hired', NULL, 'Active', 'M','Backend Developer');
-
+INSERT INTO candidates VALUES (23, 'Nikhil', 'Kumar', 'nikhil.kumar@gmail.com', 9876543232, TO_DATE('1994-09-09','YYYY-MM-DD'), 'Aadhar', 461113335355, 'BCA', 'Amity University', 7.1, 'Lucknow', 'India', 'Infosys', 550000, 750000, 5, 'PL/SQL', 'Hired', NULL, 'Active', 'M','Backend Developer');
 INSERT INTO candidates VALUES (24, 'Anjali', 'Joshi', 'anjali.joshi@gmail.com', 9876543233, TO_DATE('1993-11-11','YYYY-MM-DD'), 'DL', 123987456, 'BBA', 'PU', 7.3, 'Chandigarh', 'India', 'Flipkart', 600000, 850000, 5, 'Management', 'Rejected', 'Low CGPA', 'Inactive', 'F','HR Executive');
-
 INSERT INTO candidates VALUES (25, 'Dev', 'Patel', 'dev.patel@gmail.com', 9876543234, TO_DATE('1992-02-14','YYYY-MM-DD'), 'DL', 555666777, 'B.Tech', 'NIT Surat', 8.2, 'Surat', 'India', 'Tata Steel', 820000, 930000, 7, 'Java', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (26, 'Richa', 'Gandhi', 'richa.gandhi@gmail.com', 9876543235, TO_DATE('1994-10-20','YYYY-MM-DD'), 'Aadhar', 112233449895, 'M.Sc', 'Delhi University', 8.3, 'Delhi', 'India', 'OYO', 720000, 820000, 4, 'PL/SQL', 'In Progress', NULL, 'Active', 'F','Backend Developer');
-
 INSERT INTO candidates VALUES (28, 'Sana', 'Khan', 'sana.khan@gmail.com', 9876543237, TO_DATE('1995-03-03','YYYY-MM-DD'), 'DL', 666777888, 'B.Com', 'DU', 7.5, 'Delhi', 'India', 'Zomato', 650000, 750000, 5, 'PL/SQL', 'In Progress', NULL, 'Active', 'F','Software Engineer');
-
 INSERT INTO candidates VALUES (29, 'Karan', 'Jain', 'karan.jain@gmail.com', 9876543238, TO_DATE('1992-08-08','YYYY-MM-DD'), 'Aadhar', 444555989666, 'B.Tech', 'IIT Guwahati', 8.7, 'Guwahati', 'India', 'GAIL', 900000, 1050000, 6, 'Java', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (30, 'Shivani', 'Sharma', 'shivani.sharma@gmail.com', 9876543239, TO_DATE('1993-11-30','YYYY-MM-DD'), 'Passport', 'K3444555', 'M.Sc', 'IIT Delhi', 8.9, 'Delhi', 'India', 'DRDO', 850000, 950000, 5, 'PL/SQL', 'In Progress', NULL, 'Active', 'F','Backend Developer');
-
 INSERT INTO candidates VALUES (31, 'Varun', 'Desai', 'varun.desai@gmail.com', 9876543240, TO_DATE('2000-11-20','YYYY-MM-DD'), 'Aadhar', 998877665985, 'B.Tech', 'IIT Bombay', 9.2, 'Mumbai', 'India', NULL, NULL, 500000, 0, 'Java', 'Hired', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (32, 'Meera', 'Jain', 'meera.jain@gmail.com', 9876543241, TO_DATE('1999-09-15','YYYY-MM-DD'), 'Passport','J6566554', 'B.Sc', 'Delhi University', 8.0, 'Delhi', 'India', NULL, NULL, 450000, 0, 'Python', 'In Progress', NULL, 'Active', 'F','Software Engineer');
-
 INSERT INTO candidates VALUES (33, 'Rakesh', 'Yadav', 'rakesh.yadav@gmail.com', 9876543242, TO_DATE('2001-03-10','YYYY-MM-DD'), 'DL', 'M776623554433', 'BCA', 'Osmania University', 7.5, 'Hyderabad', 'India', NULL, NULL, 400000, 0, 'SQL', 'Hired', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (34, 'Ananya', 'Sinha', 'ananya.sinha@gmail.com', 9876543243, TO_DATE('2000-05-30','YYYY-MM-DD'), 'Aadhar', 661255443322, 'MBA', 'Amity University', 8.3, 'Noida', 'India', NULL, NULL, 600000, 0, 'HR', 'In Progress', NULL, 'Active', 'F','HR Executive');
-
 INSERT INTO candidates VALUES (35, 'Pratik', 'Shah', 'pratik.shah@gmail.com', 9876543244, TO_DATE('1998-12-25','YYYY-MM-DD'), 'Passport', 'N4332211', 'MCA', 'Anna University', 9.0, 'Chennai', 'India', NULL, NULL, 700000, 0, 'React', 'Rejected', 'Lack of technical depth', 'Inactive', 'M','Software Engineer');
-
-INSERT INTO candidates VALUES (36, 'Sana', 'Khan', 'sana.khan@gmail.com', 9876543225, TO_DATE('1997-06-18','YYYY-MM-DD'), 'Aadhar', 223344545566, 'B.Com', 'Mumbai University', 8.1, 'Mumbai', 'India', 'Infosys', 300000, 450000, 1, 'Finance', 'Hired', NULL, 'Active', 'F','Finance Analyst');
-
+INSERT INTO candidates VALUES (36, 'Sneha', 'Khan', 'sneha.khan@gmail.com', 9876513225, TO_DATE('1997-06-18','YYYY-MM-DD'), 'Aadhar', 223344545566, 'B.Com', 'Mumbai University', 8.1, 'Mumbai', 'India', 'Infosys', 300000, 450000, 1, 'Finance', 'Hired', NULL, 'Active', 'F','Finance Analyst');
 INSERT INTO candidates VALUES (37, 'Karthik', 'Rao', 'karthik.rao@gmail.com', 9876543246, TO_DATE('1996-10-12','YYYY-MM-DD'), 'Passport', 'H3556677', 'B.E', 'NIT Warangal', 8.7, 'Bangalore', 'India', 'Wipro', 550000, 700000, 2, 'Java', 'In Progress', NULL, 'Active', 'M','Software Engineer');
-
 INSERT INTO candidates VALUES (38, 'Diya', 'Mehta', 'diya.mehta@gmail.com', 9876543247, TO_DATE('2002-08-02','YYYY-MM-DD'), 'DL', 44556067788, 'B.Sc IT', 'Symbiosis', 9.0, 'Pune', 'India', NULL, NULL, 400000, 0, 'Data Analysis', 'In Progress', NULL, 'Active', 'F','System Administrator');
-
 INSERT INTO candidates VALUES (39, 'Abhinav', 'Sharma', 'abhinav.sharma@gmail.com', 9876543248, TO_DATE('1995-04-22','YYYY-MM-DD'), 'Aadhar', 556677883399, 'M.Tech', 'IIT Kanpur', 9.3, 'Kanpur', 'India', 'Tech Mahindra', 700000, 850000, 3, 'C++', 'Rejected', 'Communication skills', 'Inactive', 'M','Software Engineer');
 INSERT INTO candidates VALUES (40, 'Riya', 'Verma', 'riya.verma@gmail.com', 9876543249, TO_DATE('2001-02-14','YYYY-MM-DD'), 'Passport', 'P6677889', 'BBA', 'Christ University', 8.2, 'Bangalore', 'India', NULL, NULL, 500000, 0, 'Marketing', 'Hired', NULL, 'Active', 'F','Sales Manager');
 INSERT INTO candidates VALUES (41, 'Deepa', 'Sharma', 'deepa.sharma@example.com', 9876511111, TO_DATE('1993-07-14','YYYY-MM-DD'), 'Aadhar', 110011224333, 'MBA', 'Symbiosis', 8.1, 'Mumbai', 'India', 'Wipro', 45000, 60000, 2, 'HR, Management', 'In Progress', NULL, 'Active', 'F','HR Executive');
 INSERT INTO candidates VALUES (42, 'Ravi', 'Bansal', 'ravi.bansal@example.com', 9876522222, TO_DATE('1991-04-09','YYYY-MM-DD'), 'DL', 'DL0111223344', 'B.Com', 'Delhi University', 7.8, 'Delhi', 'India', 'Infosys', 40000, 55000, 3, 'Accounts, Excel', 'Hired', NULL, 'Active', 'M','System Administrator');
 INSERT INTO candidates VALUES (43, 'Pooja', 'Mehta', 'pooja.mehta@example.com', 9876533333, TO_DATE('1992-02-25','YYYY-MM-DD'), 'Passport', 'N1100334', 'MCA', 'IP University', 8.9, 'Noida', 'India', 'Tech Mahindra', 55000, 75000, 2, 'Java, SQL', 'Hired', NULL, 'Active', 'F','Software Engineer');
-INSERT INTO candidates VALUES (44, 'Suresh', 'Patel', 'suresh.patel@example.com', 9876544444, TO_DATE('1990-10-12','YYYY-MM-DD'), 'Aadhar', 110044587566, 'M.Tech', 'IIT Bombay', 9.3, 'Ahmedabad', 'India', 'L&T', 65000, 85000, 4, 'Marketing', 'In Progress', NULL, 'Active', 'M','Sales Manager');
+INSERT INTO candidates VALUES (44, 'Suresh', 'Patel', 'suresh.patel@example.com', 9876544444, TO_DATE('1990-10-12','YYYY-MM-DD'), 'Aadhar', 110044587566, 'M.Tech', 'IIT Bombay', 9.3, 'Ahmedabad', 'India', 'LT', 65000, 85000, 4, 'Marketing', 'In Progress', NULL, 'Active', 'M','Sales Manager');
 INSERT INTO candidates VALUES (45, 'Anjali', 'Verma', 'anjali.verma@example.com', 9876555555, TO_DATE('1994-03-19','YYYY-MM-DD'), 'DL', 'MH0110055667', 'B.Sc', 'Mumbai University', 8.2, 'Mumbai', 'India', 'Reliance', 30000, 45000, 1, 'Testing', 'Rejected', 'Not a fit', 'Inactive', 'F','QA Engineer');
 INSERT INTO candidates VALUES (46, 'Amit', 'Rao', 'amit.rao@example.com', 9876566666, TO_DATE('1989-08-23','YYYY-MM-DD'), 'Passport', 'L1100667', 'MCA', 'Osmania University', 8.5, 'Hyderabad', 'India', 'TCS', 50000, 65000, 3, 'Python, Django', 'Hired', NULL, 'Active', 'M','Backend Developer');
 INSERT INTO candidates VALUES (47, 'Kiran', 'Nair', 'kiran.nair@example.com', 9876577777, TO_DATE('1995-12-01','YYYY-MM-DD'), 'Aadhar', 110077883299, 'B.Tech', 'Anna University', 8.6, 'Chennai', 'India', 'Zoho', 38000, 52000, 1, 'JavaScript, React', 'In Progress', NULL, 'Active', 'M','Software Engineer');
@@ -362,7 +326,7 @@ INSERT INTO candidates VALUES (68, 'Rohit', 'Malik', 'rohit.malik@gmail.com', 98
 INSERT INTO candidates VALUES (69, 'Meera', 'Reddy', 'meera.reddy@gmail.com', 9876500019, TO_DATE('1998-11-29','YYYY-MM-DD'), 'DL', 'AP139300990011', 'B.Tech', 'SRM University', 8.6, 'Vijayawada', 'India', NULL, NULL, NULL, 0, 'Cloud', 'Rejected', 'Skill mismatch', 'Inactive','F','Devops Engineer');
 INSERT INTO candidates VALUES (70, 'Ayaan', 'Chopra', 'ayaan.chopra@gmail.com', 9876500020, TO_DATE('1999-02-16','YYYY-MM-DD'), 'Aadhar', 438900101022, 'M.Tech', 'NIT Warangal', 9.0, 'Warangal', 'India', NULL, NULL, NULL, 0, 'DevOps', 'In Progress', NULL, 'Active','M','Devops Engineer');
 INSERT INTO candidates VALUES (71, 'Varsha', 'Mehta', 'varsha.mehta@gmail.com', 9876583211, TO_DATE('2005-06-25','YYYY-MM-DD'), 'Aadhar', 233332211110, 'B.Tech', 'MIT Pune', 8.9, 'Mumbai', 'India', NULL, NULL, 50000, 0, 'Java', 'In Progress', NULL, 'Active', 'F','Software Engineer');
-INSERT INTO candidates VALUES (72, 'Ravi', 'Saxena', 'ravi.saxena@gmail.com', 9876243212, TO_DATE('2004-02-10','YYYY-MM-DD'), 'Passport', 'PZ234567', 'B.Sc', 'Osmania University', 7.2, 'Hyderabad', 'India', NULL, NULL, 40000, 0, 'SQL', 'In Progress', NULL, 'Active', 'M','System Administrator');
+INSERT INTO candidates VALUES (72, 'Ravi', 'Saxena', 'ravi.saxena@gmail.com', 9876243212, TO_DATE('2004-02-10','YYYY-MM-DD'), 'Passport', 'P3234567', 'B.Sc', 'Osmania University', 7.2, 'Hyderabad', 'India', NULL, NULL, 40000, 0, 'SQL', 'In Progress', NULL, 'Active', 'M','System Administrator');
 INSERT INTO candidates VALUES (73, 'Kiran', 'Nayak', 'kiran.nayak@gmail.com', 9872143213, TO_DATE('2003-08-17','YYYY-MM-DD'), 'DL', 'KA03672023001', 'BCA', 'LPU', 7.5, 'Bangalore', 'India', NULL, NULL, 45000, 0, 'Angular', 'In Progress', NULL, 'Active', 'F','Software Engineer');
 INSERT INTO candidates VALUES (74, 'Rahul', 'Jain', 'rahul.jain@gmail.com', 9816543214, TO_DATE('2005-11-01','YYYY-MM-DD'), 'Aadhar', 889977665544, 'B.Com', 'Delhi University', 8.1, 'Delhi', 'India', NULL, NULL, 47000, 0, 'Java', 'Rejected', 'Lacks practical exposure', 'Active', 'M','Software Engineer');
 INSERT INTO candidates VALUES (75, 'Sneha', 'Kapoor', 'sneha.kapoor@gmail.com', 9823543215, TO_DATE('2005-04-12','YYYY-MM-DD'), 'Passport', 'N9978877', 'B.Tech', 'SRM University', 9.0, 'Chennai', 'India', NULL, NULL, 55000, 0, 'Cloud', 'In Progress', NULL, 'Active', 'F','Devops Engineer');
@@ -372,6 +336,6 @@ INSERT INTO candidates VALUES (78, 'Tarun', 'Verma', 'tarun.verma@gmail.com', 99
 INSERT INTO candidates VALUES (79, 'Isha', 'Gupta', 'isha.gupta@gmail.com', 9865483219, TO_DATE('2004-03-18','YYYY-MM-DD'), 'DL', 'TS09DL3344556', 'B.Tech', 'IIIT Hyderabad', 8.7, 'Hyderabad', 'India', NULL, NULL, 46000, 0, 'Angular', 'In Progress', NULL, 'Active', 'F','Software Engineer');
 INSERT INTO candidates VALUES (80, 'Yash', 'Malhotra', 'yash.malhotra@gmail.com', 7876543220, TO_DATE('2005-01-25','YYYY-MM-DD'), 'Aadhar', 112233445566, 'B.Com', 'Mumbai University', 8.2, 'Mumbai', 'India', NULL, NULL, 41000, 0, 'Java', 'Rejected', 'Technical knowledge gap', 'Active', 'M','Software Engineer');
 
-SELECT * FROM candidates;
+SELECT * FROM candidates ;
 
-TRUNCATE TABLE candidates;
+--TRUNCATE TABLE candidates;
